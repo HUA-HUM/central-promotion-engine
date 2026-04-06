f12eca63e620491bd7ab750bb8db4997235ba3da
Manuel Romero <manuel@romero.com.ar>
2026-04-06 19:06:05 -0400

    Configuracion de Mongo

--- src/app/module/Mongo.module.ts
+++ src/app/module/Mongo.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { MongoPromotionRepository } from '@app/drivers/repositories/mongo/MongoPromotionRepository';
 import { Promotion, PromotionSchema } from '@core/entities/Promotion';
@@ -7,12 +8,16 @@ const repositories = [MongoPromotionRepository];
 
 @Module({
   imports: [
-    MongooseModule.forRoot(process.env.MONGO_URL as string, {
-      maxPoolSize: 10,
-      serverSelectionTimeoutMS: 5000,
-      socketTimeoutMS: 45000,
-      retryWrites: true,
-      retryReads: true,
+    MongooseModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGO_URL'),
+        maxPoolSize: 10,
+        serverSelectionTimeoutMS: 5000,
+        socketTimeoutMS: 45000,
+        retryWrites: true,
+        retryReads: true,
+      }),
     }),
     MongooseModule.forFeature([{ name: Promotion.name, schema: PromotionSchema }]),
   ],
